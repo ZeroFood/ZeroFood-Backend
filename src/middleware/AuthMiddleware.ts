2dd60f2e2fe33c3924aded2e6800901944206c76
Kanaiah Kothalikar <kanaiahk@gmail.com>
2020-04-08 21:10:15 +0000

    Fixed transpilation error

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -20,7 +20,7 @@ export class AuthMiddleware {
         }
     }
 
-    static fromHeaderOrQuerystring(req: Request) {
+    static fromHeaderOrQuerystring(req: Request): any {
         if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
             return req.headers.authorization.split(' ')[1];
         } else if (req.query && req.query.token) {
