539ddcb69bbd061634968aaab675de90912b0ba4
KANAIAH KOTHALIKAR <KKOTHALIKAR@MICRON.COM>
2020-04-12 04:04:07 +0530

    Updated authentication logic

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,15 @@ var basicAuth = require('basic-auth');
 async function initialize() {
     const app = express();
 
-    await createConnection();
+    // await createConnection();
     app.use(cors());
 
     app.use(express.json());
 
     var auth = function (req, res, next) {
         var user = basicAuth(req);
-        if (!user || !user.name || !user.pass) {
-            res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-            res.sendStatus(401);
-        }
-        if (user.name === 'admin' && user.pass === 'zerofood1') {
+        console.log(user);
+        if (user && user.name === 'admin' && user.pass === 'zerofood1') {
             next();
         } else {
             res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
